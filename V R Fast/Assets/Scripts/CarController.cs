7a5c5ce4eb219c5b62f67421e4bb1dbb23fed042
BilalElM <bilal.el.makrini1@gmail.com>
2023-05-23 14:25:41 +0200

    edited driving script

--- V R Fast/Assets/Scripts/CarController.cs
+++ V R Fast/Assets/Scripts/CarController.cs
@@ -5,87 +5,92 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-
-
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
+
     public Rigidbody rb;
     public Vector3 newCenterOfMass;
     public Transform startPosition;
 
     private float horizontalInput;
     private float verticalInput;
-    [SerializeField] private float brakeforce;
-    private bool isbreaking;
+    [SerializeField] private float brakeForce = 100000f;
+    [SerializeField] private bool isBraking;
     private float currentSteerAngle;
     private float rotationSpeed = 10000f;
-
 
     [SerializeField] private float motorForce;
     [SerializeField] private float maxSteerAngle;
 
-    [SerializeField] private WheelCollider Wheel_01;
-    [SerializeField] private WheelCollider Wheel_02;
-    [SerializeField] private WheelCollider Wheel_03;
-    [SerializeField] private WheelCollider Wheel_04;
+    public List<WheelCollider> wheels;
+    public List<Transform> wheelTransforms;
 
-    [SerializeField] private Transform Wheel_01_Transform;
-    [SerializeField] private Transform Wheel_02_Transform;
-    [SerializeField] private Transform Wheel_03_Transform;
-    [SerializeField] private Transform Wheel_04_Transform;
+    private void Start()
+    {
+        rb.centerOfMass = newCenterOfMass;
+
+        // Assign wheels automatically using the wheelTransforms list
+        for (int i = 0; i < wheels.Count; i++)
+        {
+            WheelCollider wheel = wheels[i];
+            Transform wheelTransform = wheelTransforms[i];
+
+            // Assign the wheel collider and transform to the corresponding element in the list
+            wheelTransform.rotation = wheel.transform.rotation;
+            wheelTransform.position = wheel.transform.position;
+        }
+    }
 
     private void FixedUpdate()
     {
-        //Main methodeds worden geladen
         GetInput();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
-        // Rotatie van het stuur
         RotateWheel();
-
     }
 
     public void GetInput()
     {
-        //Ik gebruik de project settings om mij auto te besturen
         horizontalInput = Input.GetAxis(HORIZONTAL);
         verticalInput = Input.GetAxis(VERTICAL);
-        isbreaking = Input.GetKey(KeyCode.Space);
-
+        isBraking = Input.GetKey(KeyCode.Space);
     }
+
     private void HandleMotor()
     {
-        //Voorwiel aandrdijving, regelt naar voor en naar achter rijden.
-        Wheel_01.motorTorque = verticalInput * motorForce;
-        Wheel_03.motorTorque = verticalInput * motorForce;
+        float motorTorque = verticalInput * motorForce;
 
-        //remmen
-        brakeforce = isbreaking ? 10000f : 0f;
-        Wheel_01.brakeTorque = brakeforce;
-        Wheel_02.brakeTorque = brakeforce;
-        Wheel_03.brakeTorque = brakeforce;
-        Wheel_04.brakeTorque = brakeforce;
+        foreach (WheelCollider wheel in wheels)
+        {
+            wheel.motorTorque = motorTorque;
+
+        }
     }
-
 
     private void HandleSteering()
     {
-        //het besturen, zorgt ervoor dat de wielen draaien.
         currentSteerAngle = maxSteerAngle * horizontalInput;
-        Wheel_01.steerAngle = currentSteerAngle;
-        Wheel_03.steerAngle = currentSteerAngle;
+
+        // Apply steering to the front wheels
+        wheels[0].steerAngle = currentSteerAngle;
+        wheels[1].steerAngle = currentSteerAngle;
+
+        // Apply braking to the rear wheels
+        wheels[0].brakeTorque = isBraking ? brakeForce : 0f;
+        wheels[1].brakeTorque = isBraking ? brakeForce : 0f;
+        wheels[2].brakeTorque = isBraking ? brakeForce : 0f;
+        wheels[3].brakeTorque = isBraking ? brakeForce : 0f;
     }
+
     private void UpdateWheels()
     {
-        //de methode die ervoor zorgt dat de wielen draaien en de auto kan bewegen.
-        UpdateSingleWheel(Wheel_01, Wheel_01_Transform);
-        UpdateSingleWheel(Wheel_02, Wheel_02_Transform);
-        UpdateSingleWheel(Wheel_03, Wheel_03_Transform);
-        UpdateSingleWheel(Wheel_04, Wheel_04_Transform);
+        for (int i = 0; i < wheels.Count; i++)
+        {
+            UpdateSingleWheel(wheels[i], wheelTransforms[i]);
+        }
     }
-    //in depth, ik neem de huidige coordinaat, update die en stuur hem terug.
-    //voor het draaien van de wielen gebruik ik Quaternion.
+
     private void UpdateSingleWheel(WheelCollider wheel, Transform wheelTransform)
     {
         Vector3 position;
@@ -94,22 +99,16 @@ public class CarController : MonoBehaviour
         wheelTransform.rotation = rotation;
         wheelTransform.position = position;
     }
+
     private void RotateWheel()
     {
-        // Roteer het stuurobject naar links of rechts
         float wheelRotation = horizontalInput * rotationSpeed * Time.deltaTime;
-        // Hier moet je de juiste naam invullen van het stuurobject in de scene
         GameObject wheelObject = GameObject.Find("sport_car_1_steering_wheel");
 
         if (wheelObject != null)
         {
-            // Haal de huidige rotatie van het stuurobject op
             Vector3 currentRotation = wheelObject.transform.localEulerAngles;
-
-            // Bereken de nieuwe rotatie op basis van het input en snelheid
             Vector3 desiredRotation = new Vector3(currentRotation.x, currentRotation.y, wheelRotation);
-
-            // Pas de rotatie van het stuurobject aan
             wheelObject.transform.localEulerAngles = desiredRotation;
         }
         else
@@ -117,12 +116,4 @@ public class CarController : MonoBehaviour
             Debug.LogWarning("Stuurobject niet gevonden!");
         }
     }
-
-    void Start()
-    {
-
-        rb.centerOfMass = newCenterOfMass;
-    }
 }
-
-
